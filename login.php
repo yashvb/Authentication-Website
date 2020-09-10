3d0d6a821941e1249ca8c50ae4d6b13c602b7b19
destroyeddesman <65105529+destroyeddesman@users.noreply.github.com>
2020-09-10 03:40:42 -0700

    1 entry
    Complete Website with Account Information Accountability

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> User Login And Registration </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+<div class='login-box'>
+    <div class="row">
+
+        <div class="col-md-6 login-left">
+            <h2> Login Here </h2>
+            <form action="validation.php" method="post">
+                <div class ="form-group">
+                    <label> Usernames </lable>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                <div class ="form-group">
+                    <label> Password </lablel>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+
+
+        <div class="col-md-6 login-right">
+            <h2> Register Here </h2>
+            <form action="registration.php" method="post">
+                <div class ="form-group">
+                    <label> Usernames </lable>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                <div class ="form-group">
+                    <label> Password </label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Registration</button>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
